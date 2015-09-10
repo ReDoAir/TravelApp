8f9eedb91dec00fcdb7ffb695f401032dba2464e
SSTAX38 <samsmets@hotmail.com>
2015-09-10 08:48:37 +0200

    Passenger adding creditcard and address

---
+++ ejb-module/src/test/java/com/realdolmen/course/persistence/CreditCard.java
@@ -0,0 +1,53 @@
+package com.realdolmen.course.persistence;
+
+import javax.persistence.Embeddable;
+
+/**
+ * Created by SSTAX38 on 10/09/2015.
+ */
+@Embeddable
+public class CreditCard {
+    private String number;
+    private String expiryDate;
+    private Integer controlNumber;
+    private CreditCardType creditCardType;
+
+    public CreditCard(String number, String expiryDate, Integer controlNumber, CreditCardType creditCardType) {
+        this.number = number;
+        this.expiryDate = expiryDate;
+        this.controlNumber = controlNumber;
+        this.creditCardType = creditCardType;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public CreditCardType getCreditCardType() {
+        return creditCardType;
+    }
+
+    public Integer getControlNumber() {
+        return controlNumber;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setControlNumber(Integer controlNumber) {
+        this.controlNumber = controlNumber;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public void setCreditCardType(CreditCardType creditCardType) {
+        this.creditCardType = creditCardType;
+    }
+}
---
+++ ejb-module/src/test/java/com/realdolmen/course/persistence/CreditCardType.java
@@ -0,0 +1,8 @@
+package com.realdolmen.course.persistence;
+
+/**
+ * Created by SSTAX38 on 10/09/2015.
+ */
+public enum CreditCardType {
+    VISA,MASTER,AMEX
+}
