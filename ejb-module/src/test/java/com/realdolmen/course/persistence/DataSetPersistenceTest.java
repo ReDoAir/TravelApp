d9dc7fb553d3355898e8afc09b3a43d294d9c1f2
SSTAX38 <samsmets@hotmail.com>
2015-09-10 09:57:57 +0200

    Passenger changes

--- ejb-module/src/test/java/com/realdolmen/course/persistence/DataSetPersistenceTest.java
+++ ejb-module/src/test/java/com/realdolmen/course/persistence/DataSetPersistenceTest.java
@@ -21,7 +21,11 @@ public abstract class DataSetPersistenceTest extends PersistenceTest {
     public void loadDataSet() throws Exception {
         logger.info("Loading dataset");
         IDataSet dataSet = new FlatXmlDataSetBuilder().build(getClass().getResource("/data.xml"));
-
+        /*
+        * <passenger id="1000" firstName="Sam" lastName="Smets" dateOfBirth = "1993-12-31" ssn="1111" frequentFlyerMiles="9999" lastFlight="2015-09-09 16:15:15" passengerType="REGULAR"/>
+          <passenger id="2000" firstName="Jos" lastName="Soj" dateOfBirth = "1982-02-01" ssn="2222" frequentFlyerMiles="8888" lastFlight="2015-09-09 16:15:15" passengerType="OCCASIONAL"/>
+          <passenger id="3000" firstName="Rik" lastName="Kir" dateOfBirth = "1985-04-01" ssn="3333" frequentFlyerMiles="7777" lastFlight="2015-09-09 16:15:15" passengerType="OCCASIONAL"/>
+        * */
         IDatabaseConnection connection = new DatabaseConnection(newConnection());
         connection.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory()); // Set factorytype in dbconfig to remove warning
         DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
