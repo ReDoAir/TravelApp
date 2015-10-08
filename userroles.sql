e7677f3d49cf8a1ff216f44b99d6e238c4129c36
sadoge <samsmets@hotmail.com>
2015-10-08 15:33:29 +0200

    sql

---
+++ userroles.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM redoair.userroles
+LIMIT 0, 50000
+
+-- Date: 2015-10-08 11:04
+*/
+INSERT INTO `userroles` (`userId`,`role`) VALUES (2,'PARTNER');
+INSERT INTO `userroles` (`userId`,`role`) VALUES (4,'EMPLOYEE');
