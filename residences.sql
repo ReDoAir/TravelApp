7b5390017e564f4df0d02f965c314e714715cb52
sadoge <samsmets@hotmail.com>
2015-10-08 16:32:49 +0200

    sqls

---
+++ residences.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM redoair.residence
+LIMIT 0, 50000
+
+-- Date: 2015-10-08 15:57
+*/
+INSERT INTO `residence` (`id`,`priceByDay`,`period_id`) VALUES (1,50,1);
---
+++ trips.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM redoair.trip
+LIMIT 0, 50000
+
+-- Date: 2015-10-08 15:57
+*/
+INSERT INTO `trip` (`id`,`destination`,`tripName`,`fromFlight_id`,`period_id`,`toFlight_id`) VALUES (1,'Athens','TestTrip',2,1,1);
